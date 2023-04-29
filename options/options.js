c6cf23409f51de4f526289af99e299b63aa7df01
Crowdbotics <team@crowdbotics.com>
2023-04-29 20:17:37 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "klplanner-40347",
+  url: "https://klplanner-40347.botics.co",
+  api: "https://klplanner-40347.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
